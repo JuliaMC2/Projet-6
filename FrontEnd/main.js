8eb73c568fafcf110bc1843cbb2f3085f88ea0a8
JuliaMC2 <juliacaudal@gmail.com>
2024-03-27 18:04:06 +0100

    modale ajout / suppr

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -35,21 +35,129 @@ if (categories === null){
     categories = JSON.parse(categories);
 }
 
+// masquage du bouton modifier si on n'est pas connecté
+let editGalleryButton = document.getElementById('editGallery');
+let filtersSection = document.querySelector('.filters');
+toggleDisplays();
+
+function toggleDisplays() {
+    if (loginOk) {
+        editGalleryButton.style.display = null;
+        filtersSection.style.display ='none';
+    } else {
+        editGalleryButton.style.display = 'none';
+        filtersSection.style.display ='flex';
+    }
+}
 //Gestion de la modale
+let modal = null;
+
+const FromGalleryToAddPhoto = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    document.getElementById('addPhotoForm').style.display = null;
+    document.getElementById('back').style.visibility = null;
+    document.getElementById('photoGallery').style.display = 'none';
+}
+
+const FromAddPhotoToGallery = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    document.getElementById('addPhotoForm').style.display = 'none';
+    document.getElementById('back').style.visibility = 'hidden';
+    document.getElementById('photoGallery').style.display = null;
+}
+
+function displayThumbs() {
+    const gallery = document.querySelector('.photoThumbsList');
+    var figure;
+    var img;
+    var item;
+
+    gallery.innerHTML = '';
+    //parcourir works et créer les éléments HTML correspondants
+    for(let i = 0; i < works.length; i++){
+        //créer la balise figure qui encapsule un élément
+        figure = document.createElement('figure');
+        //créer la balise img avec sa source et son affichage alternatif
+        img = document.createElement('img');
+        img.src=works[i].imageUrl;
+        img.alt=works[i].title;
+        //créer la poubelle, y ajouter le comportement attendu
+        item = document.createElement('i');
+        item.classList.add('fa');
+        item.classList.add('fa-trash-o');
+        item.classList.add('trashHandler');
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            (deleteProject(works[i].id));
+            if (!loginOk) {
+                window.localStorage.removeItem('token');
+            }
+        });
+        // ajouter les fils à l'élément figure
+        figure.appendChild(img);
+        figure.appendChild(item);
+        //ajouter figure à la galerie de miniatures
+        gallery.appendChild(figure);
+    }
+}
+
+async function deleteProject(idProject) {
+
+    const rawResponse = await fetch('http://localhost:5678/api/works/'+idProject, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+token
+        },
+    });
+
+    //attente de la réponse
+    console.log(rawResponse.status);
+    if (rawResponse.status === 204 ) {
+        deleteLocalProject(idProject);
+        createGallery(works);
+        displayThumbs();
+    }
+    if (rawResponse.status === 401) {
+        loginOk = false;
+        toggleDisplays();
+    }
+}
+
+function deleteLocalProject (idProject) {
+    var tempWorks = new Array(works.length-1);
+    var offset=0;
+    for (var i=0; i<works.length; i++)
+        if (works[i].id !== idProject) {
+            tempWorks[i+offset] = works[i];
+        } else {
+            offset = -1;
+        }
+    works=tempWorks;
+}
+
 const closeModal = function (e) {
     if (modal !== null) {
         e.preventDefault();
+        e.stopPropagation();
         modal.style.display = 'none';
         modal.setAttribute('aria-hidden', 'true');
         modal.setAttribute('aria-modal', 'false');
         modal.removeEventListener('click', closeModal);
         document.getElementById('exit').removeEventListener('click', closeModal);
         modal = null;
-        }
+        createCategories();
+    }
 }
 
 const openModal = function (e) {
     e.preventDefault();
+    e.stopPropagation();
+    displayThumbs();
     const target = document.getElementById("modal");
     target.style.display = null;
     target.removeAttribute('aria-hidden');
@@ -57,16 +165,17 @@ const openModal = function (e) {
     modal = target;
     modal.addEventListener('click', closeModal);
     document.getElementById('exit').addEventListener('click', closeModal);
+    document.getElementById('addPhotoButton').addEventListener('click', FromGalleryToAddPhoto);
+    document.getElementById('back').addEventListener('click', FromAddPhotoToGallery);
 }
 
 document.getElementById("modalOpener").addEventListener('click', openModal);
 
-// affichages initiaux
-createCategories();
 // affichage des projets
 createGallery(works);
-
-
+//afichage des catégories
+if (!loginOk)
+    createCategories();
 
 // affichage des filtres de catégories
 function createCategories() {
@@ -77,7 +186,7 @@ function createCategories() {
 
     //création du bouton "Tous"
     var element = document.createElement('div');
-    element.classList.add('categoryFilterButton');
+    element.classList.add('greenButton');
     element.innerHTML='Tous';
     element.addEventListener("click", function() {filterWorks("-1");});
 
@@ -87,7 +196,7 @@ function createCategories() {
     //parcours de l'ensemble des catégories et création des boutons associés
     for (let i = 0; i < categories.length; i++) {
         element = document.createElement('div');
-        element.classList.add('categoryFilterButton');
+        element.classList.add('greenButton');
         element.innerHTML=categories[i].name;
         element.addEventListener("click", function() {filterWorks(categories[i].id);});
 
